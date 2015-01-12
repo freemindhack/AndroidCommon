4f1d8da0bb17823c4d7e749bb236d2f10f15dd67
lzy <2894638601@qq.com>
2015-01-12 15:15:24 +0800

    The Pacific
    Signed-off-by: lzy <2894638601@qq.com>

---
+++ common/ImageUtils.java
@@ -0,0 +1,79 @@
+package com.cny.common;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+public class ImageUtils {
+
+    /*
+     * Scale and clip image
+     */
+    public static Bitmap zoomImg(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+        return newbm;
+    }
+
+    public static Bitmap getBitmapFromBase64(String string) {
+        byte[] bitmapArray = null;
+        try {
+            bitmapArray = Base64.decode(string, Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
+    }
+
+    public static String getBase64FromBitmap(Bitmap bitmap, int bitmapQuality) {
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, bitmapQuality, bStream);
+        byte[] bytes = bStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    public static Bitmap getBitmapFromPath(String path, int w, int h) {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        opts.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        BitmapFactory.decodeFile(path, opts);
+        int width = opts.outWidth;
+        int height = opts.outHeight;
+        float scaleWidth = 0.f, scaleHeight = 0.f;
+        if (width > w || height > h) {
+            scaleWidth = ((float) width) / w;
+            scaleHeight = ((float) height) / h;
+        }
+        opts.inJustDecodeBounds = false;
+        float scale = Math.max(scaleWidth, scaleHeight);
+        opts.inSampleSize = (int) scale;
+        WeakReference<Bitmap> weak = new WeakReference<>(BitmapFactory.decodeFile(path, opts));
+        return Bitmap.createScaledBitmap(weak.get(), w, h, true);
+    }
+
+    public static String getBase64FromPath(String path) {
+        String base64 = "";
+        try {
+            File file = new File(path);
+            byte[] buffer = new byte[(int) file.length() + 100];
+            @SuppressWarnings("resource")
+            int length = new FileInputStream(file).read(buffer);
+            base64 = Base64.encodeToString(buffer, 0, length, Base64.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return base64;
+    }
+}
---
+++ common/SDCardUtils.java
@@ -0,0 +1,64 @@
+﻿package com.cny.common;
+
+import java.io.File;
+
+import android.annotation.SuppressLint;
+import android.os.Environment;
+import android.os.StatFs;
+
+public class SDCardUtils {
+
+    private SDCardUtils() {
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    public static boolean isSDCardEnable() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+
+    }
+
+    public static String getSDCardPath() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath()
+                + File.separator;
+    }
+
+    @SuppressLint("NewApi")
+    public static long getSDCardAllSize() {
+        if (isSDCardEnable()) {
+            StatFs stat = new StatFs(getSDCardPath());
+            long availableBlocks = stat.getAvailableBlocksLong() - 4;
+            long freeBlocks = stat.getFreeBlocksLong();
+            return freeBlocks * availableBlocks;
+        }
+        return 0;
+    }
+
+    @SuppressLint("NewApi")
+    public static long getFreeBytes(String filePath) {
+        if (filePath.startsWith(getSDCardPath())) {
+            filePath = getSDCardPath();
+        } else {
+            filePath = Environment.getDataDirectory().getAbsolutePath();
+        }
+        StatFs stat = new StatFs(filePath);
+        long availableBlocks = (long) stat.getAvailableBlocksLong() - 4;
+        return stat.getBlockSizeLong() * availableBlocks;
+    }
+
+    public static String getRootDirectoryPath() {
+        return Environment.getRootDirectory().getAbsolutePath();
+    }
+
+    public static String getFileName(String pathAndName) {
+        int start = pathAndName.lastIndexOf("/");
+        int end = pathAndName.lastIndexOf(".");
+        if (start != -1 && end != -1) {
+            return pathAndName.substring(start + 1, end);
+        } else {
+            return null;
+        }
+    }
+
+
+}
